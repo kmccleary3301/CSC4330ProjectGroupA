c45c41fa6b35aabb11b013ab84549fb5dbbfbab2
Lanecsc <114027720+Lanecsc@users.noreply.github.com>
2023-03-23 14:08:43 -0500

    Added in Firebase info
    Trying to get the app to run

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBQ8jgJERC1w1W5nuxTU3MiLOCTdx4vvH8",
+  authDomain: "skill-bridge-4330.firebaseapp.com",
+  projectId: "skill-bridge-4330",
+  storageBucket: "skill-bridge-4330.appspot.com",
+  messagingSenderId: "636442520700",
+  appId: "1:636442520700:web:1456c00bf227bfc9d6edf9",
+  measurementId: "G-ZN8G3ZYX6Y"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
