ec70496e5143004fb901efed476657d5b6c43f88
Lydia Parsa <lparsa1@lsu.edu>
2023-03-23 12:28:53 -0500

    fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
       //import local font from assets folder url and use it
       Vikendi: ['url(../assets/fonts/Vikendi) format("opentype")'],
       SF: ['url(../assets/fonts/sf-compact-display-medium) format("opentype")'],
+      SFCB: ['url(../assets/fonts/sf-compact-display-bold) format("opentype")'],
+      SFCRR: ['url(../assets/fonts/sf-compact-display-rounded-regular) format("opentype")'],
+      SFCRB: ['url(../assets/fonts/sf-compact-display-rounded-bold) format("opentype")'],
+      SFCSB: ['url(../assets/fonts/sf-compact-display-semibold) format("opentype")'],
+      SFCR: ['url(../assets/fonts/sf-compact-display-regular) format("opentype")'],
     },
       apple: {
     extend: {},
