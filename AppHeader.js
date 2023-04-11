7558f1a912d6c794bd09e0a78f1ed7bfb338f1d3
jakebuddy7 <65054338+jakebuddy7@users.noreply.github.com>
2023-04-11 08:59:19 -0500

    header optimized (logo finally centered)

--- AppHeader.js
+++ AppHeader.js
@@ -1,67 +1,120 @@
 import React from 'react';
-import { View, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { Badge, Surface, Title } from 'react-native-paper';
 
-const AppHeader = ({ onHamburgerPress }) => {
-    return (
-      <View style={styles.container}>
-        <TouchableOpacity onPress={onHamburgerPress} style={styles.menuButton}>
+const IconSize = 24;
+const blue = '#182640';
+const tan = '#FAE8CD'; 
+const lightBlue = '#C9D3FF';
+
+const AppHeader = ({
+  style,
+  menu,
+  back,
+  title,
+  right,
+  // onRightPress,
+  // optionalBtn,
+  // optionalBtnPress,
+  // rightComponent,
+  headerBg,
+  iconColor,
+  // titleAlight,
+  // optionalBadge,
+  tintColor,
+  onMenuPress,
+  onLogoutPress,
+}) => {
+  const LeftView = () => (
+    <View style={styles.view}>
+      {menu && (
+        <TouchableOpacity onPress={onMenuPress}>
           <Image
             source={require('./assets/icons/menu.png')}
-            style={styles.menuIcon}
+            style={{ width: IconSize, height: IconSize, tintColor: iconColor }}
+            resizeMode="contain"
           />
         </TouchableOpacity>
-        <View style={styles.logoWrapper} />
+      )}
+      {back && (
+        <TouchableOpacity onPress={() => {}}>
+          <Image
+            source={require('./assets/icons/back.png')}
+            style={{ width: IconSize, height: IconSize, tintColor: iconColor }}
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+      )}
+    </View>
+  );
+
+  const RightView = () => (
+    <View style={[styles.view, styles.rightView]} />
+  );
+  
+
+  const TitleView = () => (
+    <View style={styles.titleView}>
+      {!title && (
         <Image
-          source={require('./assets/logos/blue.png')}
-          style={styles.logo}
+          source={require('./assets/icons/more.png')}
+          style={{ ...styles.logo, tintColor }}
+          resizeMode="contain"
         />
-        <View />
-      </View>
-    );
-  };
-  
-  
-  
+      )}
+      {title && (
+        <Image
+        source={require('./assets/logos/blue.png')}
+        style={{ ...styles.logo, tintColor }}
+        resizeMode="contain"
+      />
+      )}
+    </View>
+  );
   
   
 
-  const styles = StyleSheet.create({
-    container: {
-      flexDirection: 'row',
-      justifyContent: 'start',
-      alignItems: 'center',
-      width: '100%',
-    },
-    menuButton: {
-      marginLeft: 10,
-    },
-    menuIcon: {
-      width: 24,
-      height: 24,
-      marginLeft: 20,
-    },
-    logo: {
-      position: 'absolute',
-      width: 60,
-      height: 60,
-      resizeMode: 'contain',
-      left: 0,
-      right: 0,
-      top: 0,
-      bottom: 0,
-      marginHorizontal: 'auto',
-      marginVertical: 'auto',
-
-    },
-    spacer: {
-      width: 45,
-    },
-    logoWrapper: {
-      width: 45,
-    },
-    
-  });
-  
-  
+  return (
+    <Surface
+      style={[styles.header, style, { backgroundColor: headerBg || tan }]}
+    >
+      <LeftView />
+      <TitleView />
+      <RightView />
+    </Surface>
+  );
+};
 
 export default AppHeader;
+
+const styles = StyleSheet.create({
+  header: {
+    height: 60,
+    elevation: 8,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  view: {
+    marginHorizontal: 16,
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  titleView: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  rightView: {
+    justifyContent: 'flex-end',
+  },
+  rowView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginRight: 10,
+  },
+  logo: {
+    width: 60,
+    height: 60,
+    marginRight: 50,
+  },
+});
