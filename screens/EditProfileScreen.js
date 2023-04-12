058fa0e997885ac245f84e04abd0806440bdcdf5
Lydia Parsa <lparsa1@lsu.edu>
2023-04-11 19:50:14 -0500

    yes

--- screens/EditProfileScreen.js
+++ screens/EditProfileScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 import ProfileScreen from "./ProfileScreen";
 import { useNavigation } from "@react-navigation/native";
 import NavBarContainer from '../NavBar';
@@ -8,6 +8,10 @@ import NavBarContainer from '../NavBar';
 
 const EditProfileScreen = () => {
     const navigation = useNavigation();
+
+    const handleCancel = () => {
+        navigation.navigate(ProfileScreen);
+    }
   
 //   // Access the passed user profile information from the route prop
 //   const { userProfile } = route.params;
@@ -30,14 +34,15 @@ const EditProfileScreen = () => {
 return (
     <View style={{ flex: 1 }}>
     <View style={styles.container}></View>
+
+    <Pressable
+        onPress = {handleCancel}>
+        <Text style={styles.cancelText}>Cancel</Text>
+    </Pressable>
     <NavBarContainer />
   </View>
 );
 };
-
-
-
-
 
 
 
@@ -55,12 +60,13 @@ return (
 //         onChangeText={setEmail}
 //         placeholder="Email"
 //       />
-//       {/* Render other editable form fields */}
 //       <Button title="Save" onPress={handleSaveProfile} />
 //     </View>
 //   );
 // };
 
+const blue = '#182640';
+const tan = '#FAE8CD'; 
 
 const styles = StyleSheet.create({
     container: {
@@ -70,6 +76,13 @@ const styles = StyleSheet.create({
         backgroundColor: '#182640',
         paddingTop: 120,
       },
+    cancelText: {
+        fontFamily: 'SF',
+        fontSize: 15,
+        color: tan,
+        backgroundColor: blue,
+        padding: 5,
+    },
 });
 
 
