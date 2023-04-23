24b5ed216a580be8b47daf46e0c3377e937b91bb
Lanecsc <114027720+Lanecsc@users.noreply.github.com>
2023-04-23 17:43:00 -0500

    Fixed SubjectAddScreen

--- src/screens/SubjectAddScreen.js
+++ src/screens/SubjectAddScreen.js
@@ -10,23 +10,52 @@ import {
 import { useNavigation } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
 import styles from '../../styles.js';
+import { auth, db } from '../../firebase';
+import { doc, setDoc, updateDoc, getDoc } from "firebase/firestore";
+import { useAuthValue } from '../../AuthContext.js';
+import { subjectList } from '../utils/subjectList.js';
 
 
-const SubjectAddScreen = ({ navigation }) => {
+  const SubjectAddScreen = ({ navigation }) => {
+    const {currentUser} = useAuthValue();
+    const [selectedSubjects, setSelectedSubjects] = useState([]);
 
-
-
-
-  const [email, setEmail] = useState('');
-
-  const [userType, setUserType] = useState('');
-
-  const [pronouns, setPronouns] = useState('');
-
-  const handleNext = () => {
-
-    navigation.navigate('HomeScreen');
-  };
+    const handleSubjectSelection = (value) => {
+      if (selectedSubjects.includes(value)) {
+        setSelectedSubjects((prev) => prev.filter((subject) => subject !== value));
+      } else {
+        setSelectedSubjects((prev) => [...prev, value]);
+      }
+    };
+  
+    const handleSaveChanges = async () => {
+      try {
+        const user = auth.currentUser;
+        await updateDoc(doc(db, 'users', user?.uid), { selectedSubjects });
+        navigation.navigate('HomeScreen');
+      } catch (err) {
+        console.log(err);
+        alert('Error saving changes.');
+      }
+    };
+    const renderPicker = () => {
+      return (
+        <Picker
+          selectedValue={''}
+          onValueChange={(value) => handleSubjectSelection(value)}
+        >
+          <Picker.Item label="Select a subject" value="" />
+          {subjectList.map((subject) => (
+            <Picker.Item
+              key={subject.value || subject}
+              label={subject.value || subject}
+              value={subject.value || subject}
+            />
+          ))}
+        </Picker>
+      );
+    };
+  
 
   return (
     <View style={[styles.container, { justifyContent: 'start', paddingTop: 120, marginTop: 0 }]}>
@@ -42,49 +71,21 @@ const SubjectAddScreen = ({ navigation }) => {
           />
         </View>
       </View>
-      <Text style={[styles.title, { textAlign: 'center', marginBottom: 0, paddingBottom: 0 }]}>Register</Text>
+      <Text style={[styles.title, { textAlign: 'center', marginBottom: 0, paddingBottom: 0 }]}>Subjects of Interest</Text>
       <View style={styles.registerBody}>
         <Text style={[styles.subtitle, { textAlign: 'center', marginBottom: 16 }]}>
           Lastly, you'll just need to add your subjects of interest.
         </Text>
-        <Picker
-          style={[styles.picker, { paddingLeft: 5 }]}
-          selectedValue={userType}
-          onValueChange={(itemValue) => setUserType(itemValue)}
-          prompt="I am a..."
-          mode="dropdown"
-        >
-          <Picker.Item label="I am a..." value="" />
-          <Picker.Item label="Student" value="student" />
-          <Picker.Item label="Tutor" value="tutor" />
-          <Picker.Item label="Administrator" value="administrator" />
-        </Picker>
-        <Picker
-          style={[styles.picker, { paddingLeft: 5 }]}
-          selectedValue={pronouns}
-          onValueChange={(itemValue) => setPronouns(itemValue)}
-          prompt="My pronouns are..."
-          mode="dropdown"
-        >
-          <Picker.Item label="My pronouns are..." value="" />
-          <Picker.Item label="He/him" value="He/him" />
-          <Picker.Item label="She/her" value="She/her" />
-          <Picker.Item label="They/them" value="They/them" />
-          <Picker.Item label="He/they" value="He/they" />
-          <Picker.Item label="She/they" value="She/they" />
-        </Picker>
+        {selectedSubjects.map((subject) => (
+        <Text key={subject}>{subject}</Text>
+      ))}
+      {selectedSubjects.length < 5 && renderPicker()}
+      <TouchableOpacity onPress={handleSaveChanges}>
+        <Text>Save Changes</Text>
+      </TouchableOpacity>
 
-        <TouchableOpacity onPress={handleNext}
-          style={[styles.button, styles.loginButton, { width: '50%' }]}
-        >
-          <Text style={styles.buttonText}>Finish</Text>
-        </TouchableOpacity>
-        <View style={[styles.linkContainer, { marginTop: 16 }]}>
-          <Text style={styles.linkText}>Already have an account?</Text>
-          <TouchableOpacity onPress={() => navigation.navigate('LoginScreen')}>
-            <Text style={styles.link}>Login</Text>
-          </TouchableOpacity>
-        </View>
+        
+        
       </View>
     </View>
   );
@@ -105,3 +106,70 @@ SubjectAddScreen.navigationOptions = ({ navigation }) => ({
 });
 
 export default SubjectAddScreen;
+
+
+// These are old, but may need to be used in other files
+
+// const handleSubmission = () => {
+  //   // Store selected subjects in Firebase
+  //   firebase.firestore().collection('selectedSubjects').add({
+  //     subjects: selectedSubjects
+  //   })
+  //     .then(() => {
+  //       alert('Selected subjects stored in Firebase!');
+  //       setSelectedSubjects([]);
+  //     })
+  //     .catch(error => {
+  //       console.error('Error storing selected subjects:', error);
+  //       alert('Error storing selected subjects');
+  //     });
+  // };
+    // let handleSubjectChange = (e) => {
+    //   setSubjects(e.target.value)
+    // }
+
+    // const handleSubjectMax = (subject) => {
+    //   if (userSubjects.length >= 5) {
+    //     alert("You have already added the maximum number of subjects.");
+    //     return;
+    //   }
+
+    //   if (!userSubjects.includes(subject)) {
+    //     const updatedUserSubjects = [...userSubjects, subject];
+    //     setUserSubjects(updatedUserSubjects);
+    //     setShowDropdown(false);
+    //   } else {
+    //     alert(`You have already added "${subject}" as one of your subjects.`);
+    //   }
+    // };
+  //     const handleNext = () => {
+
+  //       navigation.navigate('HomeScreen');
+  //     };
+
+  //       const updatecurrentUser = e => {
+  //   e.preventDefault();
+  //   handleSaveChanges(
+  //     firstName,
+  //     lastName,
+  //     email,
+  //     pronouns,
+      
+  //   ).then(() => {
+  //     navigation.navigate('ProfileScreen')
+  //   });
+  // }
+
+  // const handleSaveChanges = async (firstName,lastName,email,pronouns) => {
+  //     try {
+  //       await updateDoc(doc(db, "users", user?.uid), {
+  //         firstName,
+  //         lastName,
+  //         email,
+  //         pronouns          
+  //       });
+  //     } catch (err) {
+  //       console.log(err);
+  //       alert(`Error saving changes.`);
+  //     }
+  //   };
