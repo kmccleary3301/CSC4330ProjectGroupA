8997b80ccea55a366d37ebefe894aee2c20b617c
kmccleary3301 <kmccl24@lsu.edu>
2023-04-23 16:37:05 -0500

    MEGA REFACTOR

--- Podfile
+++
@@ -1,9 +0,0 @@
-workspace 'SkillBridgeApp'
-
-platform :ios, '12.0'
-
-target 'SkillBridge' do
-  use_frameworks!
-  pod 'Alamofire', '~> 5.4'
-  pod 'SwiftyJSON', '~> 4.0'
-end
---
+++ src/screens/SettingsScreen.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
+import styles from '../../styles.js';
+
+
+const SettingsScreen = ({ navigation }) => {
+    const [theme, setTheme] = useState('');
+    const [font, setFont] = useState('');
+}
